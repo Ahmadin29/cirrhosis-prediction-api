342536162f8bb63c92239ea4564681def5eaad79
Ahmad Saefudin <ahmaddin211056@gmail.com>
2024-09-10 01:14:41 +0700

    [chore] Add feature enginering and data model prepraration

---
+++ helpers/feature_engine.py
@@ -0,0 +1,88 @@
+# Bilirubin_Albumin_Ratio = None
+# SGOT_Ratio = None
+# Copper_Albumin_Ratio = None
+# Platelets_Bilirubin_Ratio = None
+# Tryglicerides_Albumin_Ratio = None
+# Age_Category = None
+# Bilirubin_Category = None
+# Albumin_Category = None
+# Copper_Category = None
+# Tryglicerides_Category = None
+# Platelets_Category = None
+# Prothrombin_Category = None
+from request import Request
+
+class FeatureEngine:
+    def __init__(self, basic_features):
+        self.data = basic_features
+
+    def calculate_bilirubin_albumin_ratio(self):
+        return self.data["Bilirubin"] / self.data["Albumin"]
+    
+    def calculate_sgot_ratio(self):
+        return self.data["SGOT"] / self.data["Alk_Phos"]
+    
+    def calculate_copper_albumin_ratio(self):
+        return self.data["Copper"] / self.data["Albumin"]
+    
+    def calculate_platelets_bilirubin_ratio(self):
+        return self.data["Platelets"] / self.data["Bilirubin"]
+    
+    def calculate_troglicerides_albumin_ratio(self):
+        return self.data["Tryglicerides"] / self.data["Albumin"]
+    
+    def calculate_age_category(self):
+        if self.data['Age'] <= 30:
+            return 1
+        elif self.data['Age'] <= 50:
+            return 2
+        else:
+            return 3
+        
+    def calculate_bilirubin_category(self):
+        if 0.3 <= self.data['Bilirubin'] <= 1.2:
+            return 0
+        elif self.data['Bilirubin'] > 1.2:
+            return 1
+        else:
+            return 0
+        
+    def calculate_albumin_category(self):
+        if 3.5 < self.data['Albumin'] <= 5:
+            return 0
+        elif self.data['Albumin'] <= 3.5:
+            return 1
+        else:
+            return 0
+        
+    def calculate_copper_category(self):
+        if 70 < self.data['Copper'] <= 140:
+            return 0
+        elif self.data['Copper'] <= 70 or self.data['Copper'] > 140:
+            return 1
+        else:
+            return 0
+        
+    def calculate_troglicerides_category(self):
+        if self.data['Tryglicerides'] <= 150:
+            return 0
+        elif self.data['Tryglicerides'] > 150:
+            return 1
+        else:
+            return 0
+        
+    def calculate_platelets_category(self):
+        if 150 < self.data['Platelets'] <= 450:
+            return 0
+        elif self.data['Platelets'] <= 150 or self.data['Platelets'] > 450:
+            return 1
+        else:
+            return 0
+        
+    def calculate_prothrombin_category(self):
+        if 11 <= self.data['Prothrombin'] <= 13.5:
+            return 0
+        elif self.data['Prothrombin'] < 11 or self.data['Prothrombin'] > 13.5:
+            return 1
+        else:
+            return 0
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from request import Request
-
 from fastapi import FastAPI # type: ignore
+from request import Request
+import pandas as pd
+from helpers.feature_engine import FeatureEngine
 
 app = FastAPI()
 
@@ -10,8 +11,57 @@ def read_root():
 
 @app.post("/predict")
 async def predict(request: Request):
+
+    basic_features = request.dict()
+    feature_engine = FeatureEngine(basic_features)
+
+    Bilirubin_Albumin_Ratio         = feature_engine.calculate_bilirubin_albumin_ratio()
+    SGOT_Ratio                      = feature_engine.calculate_sgot_ratio()
+    Copper_Albumin_Ratio            = feature_engine.calculate_copper_albumin_ratio()
+    Platelets_Bilirubin_Ratio       = feature_engine.calculate_platelets_bilirubin_ratio()
+    Tryglicerides_Albumin_Ratio     = feature_engine.calculate_troglicerides_albumin_ratio()
+    Age_Category                    = feature_engine.calculate_age_category()
+    Bilirubin_Category              = feature_engine.calculate_bilirubin_category()
+    Albumin_Category                = feature_engine.calculate_albumin_category()
+    Copper_Category                 = feature_engine.calculate_copper_category()
+    Tryglicerides_Category          = feature_engine.calculate_troglicerides_category()
+    Platelets_Category              = feature_engine.calculate_platelets_category()
+    Prothrombin_Category            = feature_engine.calculate_prothrombin_category()
+
+    data_to_predict = {
+        **basic_features,
+        "Bilirubin_Albumin_Ratio": Bilirubin_Albumin_Ratio,
+        "SGOT_Ratio": SGOT_Ratio,
+        "Copper_Albumin_Ratio": Copper_Albumin_Ratio,
+        "Platelets_Bilirubin_Ratio": Platelets_Bilirubin_Ratio,
+        "Tryglicerides_Albumin_Ratio": Tryglicerides_Albumin_Ratio,
+        "Age_Category": Age_Category,
+        "Bilirubin_Category": Bilirubin_Category,
+        "Albumin_Category": Albumin_Category,
+        "Copper_Category": Copper_Category,
+        "Tryglicerides_Category": Tryglicerides_Category,
+        "Platelets_Category": Platelets_Category,
+        "Prothrombin_Category": Prothrombin_Category
+    }
+
+    dataframe_to_predict = pd.DataFrame([data_to_predict])
+
     return {
         "status":"success",
         "message":"Prediction Successful",
-        "data":request
+        "data":{
+            **basic_features,
+            "Bilirubin_Albumin_Ratio": Bilirubin_Albumin_Ratio,
+            "SGOT_Ratio": SGOT_Ratio,
+            "Copper_Albumin_Ratio": Copper_Albumin_Ratio,
+            "Platelets_Bilirubin_Ratio": Platelets_Bilirubin_Ratio,
+            "Tryglicerides_Albumin_Ratio": Tryglicerides_Albumin_Ratio,
+            "Age_Category": Age_Category,
+            "Bilirubin_Category": Bilirubin_Category,
+            "Albumin_Category": Albumin_Category,
+            "Copper_Category": Copper_Category,
+            "Tryglicerides_Category": Tryglicerides_Category,
+            "Platelets_Category": Platelets_Category,
+            "Prothrombin_Category": Prothrombin_Category
+        }
     }
